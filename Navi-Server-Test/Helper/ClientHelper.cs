36c634abe8f64a629baf5e5d0351a52b37c29329
KangDroid <hyunwoo286@gmail.com>
2021-08-28 18:16:33 +0900

    Controller: Add API for registering user

---
+++ Navi-Server-Test/Helper/ClientHelper.cs
@@ -0,0 +1,53 @@
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace Navi_Server_Test.Helper
+{
+    public static class ClientHelper
+    {
+        public static async Task<ResponseEntity<T>> GetForEntity<T>(this HttpClient httpClient, string url,
+            string token)
+        {
+            httpClient.DefaultRequestHeaders.Clear();
+            // httpClient.DefaultRequestHeaders.Add("X-API-AUTH", token);
+            var response = await httpClient.GetAsync(url);
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            return new ResponseEntity<T>
+            {
+                Body = JsonConvert.DeserializeObject<T>(jsonString),
+                StatusCode = (int) response.StatusCode,
+                RawMessage = response
+            };
+        }
+
+        public static async Task<ResponseEntity<T>> PostForEntity<T, TB>(this HttpClient httpClient, string url,
+            TB toPost, string token)
+        {
+            // Add Token to header if exists.
+            httpClient.DefaultRequestHeaders.Clear();
+            // httpClient.DefaultRequestHeaders.Add("X-API-AUTH", token);
+
+            // Create Request Body
+            var requestContent = new StringContent(
+                JsonSerializer.Serialize(toPost),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            // Request && Deserialize
+            var response = await httpClient.PostAsync(url, requestContent);
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            return new ResponseEntity<T>
+            {
+                Body = JsonConvert.DeserializeObject<T>(jsonString),
+                StatusCode = (int) response.StatusCode,
+                RawMessage = response
+            };
+        }
+    }
+}
---
+++ Navi-Server/Models/ErrorResponseModel.cs
@@ -0,0 +1,8 @@
+namespace Navi_Server.Models
+{
+    public class ErrorResponseModel
+    {
+        public string TraceId { get; set; }
+        public string Message { get; set; }
+    }
+}
